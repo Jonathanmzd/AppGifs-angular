860357642284c9e9d6c5965f285be6a31ffcf6d3
Jonathan Ardila <mized2009@gmail.com>
2021-07-02 11:33:44 -0500

    84. Mostrar los resultados en pantalla

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,6 +9,9 @@ export class GifsService {
   private apiKey: string = '5XgIGRbM12WAqj5iMQIkoOkERlrRm0UF';
   private _historial:string[] = [];
 
+  // TODO: Cambiar any por su tipo
+  public resultados: any[]=[];
+
   get historial() {
     return [...this._historial];
   }
@@ -26,9 +29,10 @@ export class GifsService {
     }
 
     // llamado de la api desde un observable
-    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=5XgIGRbM12WAqj5iMQIkoOkERlrRm0UF&q=dragon&limit=10')
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=5XgIGRbM12WAqj5iMQIkoOkERlrRm0UF&q=${query}&limit=10`)
     .subscribe((resp:any) => {
-      console.log(resp);
+      console.log(resp.data);
+      this.resultados = resp.data;
     })
 
    
